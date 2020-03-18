04e4ee661068e3a58a991aafe194987115b8c3b3
IrtesamTheHack <irtesmam@gmail.com>
2020-03-18 12:36:51 +0200

    set up everything

---
+++ src/Calculator.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import './App.scss';
+
+function Calculator() {
+    return (
+        <div className="section has-background-black">
+            <div className="hero is-fullheight">
+		        <div className="hero-body">
+			        <div className="container">
+				        <div className="columns is-centered">
+                            <div className="column is-half-desktop is-three-quarters-tablet">
+                                    <div className="container">
+                                        <div className="columns is-multiline is-half-desktop is-centered">
+                                            <div className="column">
+                                                <button className="button is-primary">agagg</button>
+                                            </div>
+                                            <div className="column">
+                                                <button className="button is-primary">agagg</button>
+                                            </div>
+                                            <div className="column">
+                                                <button className="button is-primary">agagg</button>
+                                            </div>
+                                            <div className="column">
+                                                <button className="button is-primary">agagg</button>
+                                            </div>
+                                            <div className="column">
+                                                <button className="button is-primary">agagg</button>
+                                            </div>
+                                            <div className="column">
+                                                <button className="button is-primary">agagg</button>
+                                            </div>
+                                            <div className="column">
+                                                <button className="button is-primary">agagg</button>
+                                            </div>
+                                            <div className="column">
+                                                <button className="button is-primary">agagg</button>
+                                            </div>
+                                            <div className="column">
+                                                <button className="button is-primary">agagg</button>
+                                            </div>
+                                            <div className="column">
+                                                <button className="button is-primary">agagg</button>
+                                            </div>
+                                            <div className="column">
+                                                <button className="button is-primary">agagg</button>
+                                            </div>
+                                            <div className="column">
+                                                <button className="button is-primary">agagg</button>
+                                            </div>
+                                            <div className="column">
+                                                <button className="button is-primary">agagg</button>
+                                            </div>
+                                            <div className="column">
+                                                <button className="button is-primary">agagg</button>
+                                            </div>
+                                            <div className="column">
+                                                <button className="button is-primary">agagg</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Calculator;
